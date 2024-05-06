cd80e140652e6753697510bee1194e3a0f7362ad
Bojken07 <bojken.zaimi@stud.sti.se>
2024-05-06 14:18:17 +0200

    first commit

---
+++ src/main/java/se/bojken/com/projektuppgift/ProjektuppgiftApplication.java
@@ -0,0 +1,13 @@
+package se.bojken.com.projektuppgift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjektuppgiftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjektuppgiftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/bojken/com/projektuppgift/ProjektuppgiftApplicationTests.java
@@ -0,0 +1,13 @@
+package se.bojken.com.projektuppgift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjektuppgiftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
